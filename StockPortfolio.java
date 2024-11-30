0b8c27931302f3b91f855c4a726316350750afc9
Umapavani <40134826+Umapavani@users.noreply.github.com>
2024-11-30 15:22:14 +0530

    Implemented StockPortifolio

--- StockPortfolio.java
+++ StockPortfolio.java
@@ -1,29 +1,38 @@
 class StockPortfolio {
-    // TODO: Add necessary fields for stock details
+    
     private String symbol;
     private int quantity;
     private double buyingPrice;
     private double currentPrice;
+    private double highestPrice;
 
     // TODO: Initialize stock details
     public StockPortfolio(String symbol, int quantity, double buyingPrice) {
-        // TODO
+        this.symbol = symbol;
+        this.quantity= quantity;
+        this.buyingPrice = buyingPrice;
+        this.currentPrice = buyingPrice;
+        this.highestPrice = buyingPrice;
     }
 
     // TODO: Update current market price
     public void updatePrice(double newPrice) {
-        // TODO
+        if(newPrice>0){
+            if(newPrice>this.higestPrice){
+                this.highestPrice = newPrice;
+            }
+        this.currentPrice = newPrice;
+        }
     }
 
     // TODO: Calculate total profit or loss
     public double calculateProfit() {
-        // TODO
-        return 0.0;
+    
+        return (this.currentPrice - this.buyingPrice)*this.quantity;
     }
 
     // TODO: Calculate current total value of holdings
     public double getCurrentValue() {
-        // TODO
-        return 0.0;
+        return this.currentPrice * this.quantity;
     }
 }
